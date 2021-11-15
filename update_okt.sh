76830905eccd2438fa91a38bab003f39b2798824
Keunyoung-Jung <jgy206@gmail.com>
2021-11-15 14:34:38 +0900

    Create update_okt.sh

---
+++ update_okt.sh
@@ -0,0 +1,4 @@
+cd /opt/conda/lib/python3.9/site-packages/konlpy/java/
+
+rm -rf open-korean-text-2.1.0.jar
+wget https://github.com/Keunyoung-Jung/sns-tokenizer-whole/releases/download/6.5.2/open-korean-text-2.1.0.jar
